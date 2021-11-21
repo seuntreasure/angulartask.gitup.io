20b0a12dc3485a2075448746c77ead595f867c3f
Oluwaseun Aderintoye <a.seuntreasure@gmail.com>
2021-11-21 14:13:32 +0100

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PreLoaderComponent } from 'src/app/shared/pre-loader/pre-loader.component';
+import { HeaderComponent } from './header/header.component';
+import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
+import { LeftSideBarComponent } from 'src/app/shared/left-side-bar/left-side-bar.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    PreLoaderComponent,
+    HeaderComponent,
+    RightSidebarComponent,
+    LeftSideBarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    PreLoaderComponent,
+    HeaderComponent,
+    RightSidebarComponent,
+    LeftSideBarComponent,
+    FooterComponent,
+    CommonModule
+  ]
+})
+export class SharedModule { }
